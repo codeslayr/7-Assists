1f139df38e9da5b06d841a493be7a3b0669ab74e
Weirdo <ansariabdurrahman@gmail.com>
2024-07-28 11:46:51 +0530

    resolving issue - 1st iteration

--- lib/screens/mobile_view/empower_veiw_mobile.dart
+++ lib/screens/mobile_view/empower_veiw_mobile.dart
@@ -13,76 +13,71 @@ class EmpowerViewMobile extends StatelessWidget {
       child: Container(
         padding: EdgeInsets.symmetric(vertical: screenHeight / 10),
         width: screenWidth,
-        // height: screenHeight,
         color: WhitebackgroundCustomcolor,
-        child: Column(
-          children: [
-            Padding(
-              padding: EdgeInsets.only(left: screenWidth / 25),
-              child: Row(
-                children: [
-                  Container(
-                    height: 60,
-                    width: 60,
-                    child: Image.asset(
-                      "assets/images/square.png",
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 16,
-                  ),
-                  Text(
-                    "Empower",
-                    style: kSectionHeadingTextStyle.copyWith(
-                        color: Colors.grey.shade400, fontSize: 70),
-                  )
-                ],
-              ),
-            ),
-            SizedBox(
-              height: screenHeight / 40,
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: screenWidth / 20),
-              child: Text(
-                'Solutions that Fit Your Budget and Needs. We understand the challenges faced by tier 3 or low-level clubs with limited resources. Our website development and social media post designing service is tailored to address your pain points while ensuring affordability and quality. With us, you can enhance your online presence without breaking the bank.',
-                textAlign: TextAlign.start,
-                style: kSectionSubheadingTextStyle.copyWith(
-                    fontSize: 28, fontWeight: FontWeight.w400),
-              ),
-            ),
-            SizedBox(
-              height: screenHeight / 20,
-            ),
-            Column(
-              children: [
-                Stack(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(left: screenWidth / 25),
+                child: Row(
                   children: [
-                    Positioned(
-                      top: 100,
-                      left: 6,
-                      height: 70,
-                      width: 72,
+                    Container(
+                      height: 60,
+                      width: 60,
                       child: Image.asset(
-                        "assets/images/rectangle.png",
-                        // fit: BoxFit.cover,
+                        "assets/images/square.png",
                       ),
                     ),
-                    Padding(
-                      padding: EdgeInsets.only(left: screenWidth / 10),
-                      child: Container(
-                        padding: EdgeInsets.only(top: screenHeight / 8),
-                        width: 400,
-                        height: 400,
-                        // color: Colors.grey.shade300,
-                        child: const Positioned.fill(
+                    const SizedBox(
+                      width: 16,
+                    ),
+                    Text(
+                      "Empower",
+                      style: kSectionHeadingTextStyle.copyWith(
+                          color: Colors.grey.shade400, fontSize: 70),
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: screenHeight / 40,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: screenWidth / 20),
+                child: Text(
+                  'Solutions that Fit Your Budget and Needs. We understand the challenges faced by tier 3 or low-level clubs with limited resources. Our website development and social media post designing service is tailored to address your pain points while ensuring affordability and quality. With us, you can enhance your online presence without breaking the bank.',
+                  textAlign: TextAlign.start,
+                  style: kSectionSubheadingTextStyle.copyWith(
+                      fontSize: 28, fontWeight: FontWeight.w400),
+                ),
+              ),
+              SizedBox(
+                height: screenHeight / 20,
+              ),
+              Column(
+                children: [
+                  Stack(
+                    children: [
+                      Positioned(
+                        top: 100,
+                        left: 6,
+                        height: 70,
+                        width: 72,
+                        child: Image.asset(
+                          "assets/images/rectangle.png",
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(left: screenWidth / 10),
+                        child: Container(
+                          padding: EdgeInsets.only(top: screenHeight / 8),
+                          width: 400,
                           child: Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
                               Text(
                                 "Professional Club Website",
                                 style: mSectionHeadingTextStyle,
-                                // textAlign: TextAlign.center,
                               ),
                               SizedBox(
                                 height: 16,
@@ -90,50 +85,43 @@ class EmpowerViewMobile extends StatelessWidget {
                               Text(
                                 "Weekly scheduled post designs, maintained professional website to help you be the best in your league!",
                                 style: kSectionSubheadingTextStyle,
-                                // textAlign: TextAlign.center,
                               ),
                             ],
                           ),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-                Image.asset("assets/images/website2.png"),
-              ],
-            ),
-            SizedBox(
-              height: screenHeight / 20,
-            ),
-            Column(
-              children: [
-                Stack(
-                  children: [
-                    Positioned(
-                      top: 100,
-                      left: 6,
-                      height: 70,
-                      width: 72,
-                      child: Image.asset(
-                        "assets/images/rectangle.png",
-                        // fit: BoxFit.cover,
+                    ],
+                  ),
+                  Image.asset("assets/images/website2.png"),
+                ],
+              ),
+              SizedBox(
+                height: screenHeight / 20,
+              ),
+              Column(
+                children: [
+                  Stack(
+                    children: [
+                      Positioned(
+                        top: 100,
+                        left: 6,
+                        height: 70,
+                        width: 72,
+                        child: Image.asset(
+                          "assets/images/rectangle.png",
+                        ),
                       ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(left: screenWidth / 10),
-                      child: Container(
-                        padding: EdgeInsets.only(top: screenHeight / 8),
-                        width: 400,
-                        height: 400,
-                        // color: Colors.grey.shade300,
-                        child: const Positioned.fill(
+                      Padding(
+                        padding: EdgeInsets.only(left: screenWidth / 10),
+                        child: Container(
+                          padding: EdgeInsets.only(top: screenHeight / 8),
+                          width: 400,
                           child: Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
                               Text(
                                 "Social Media Designs",
                                 style: mSectionHeadingTextStyle,
-                                // textAlign: TextAlign.center,
                               ),
                               SizedBox(
                                 height: 16,
@@ -141,24 +129,24 @@ class EmpowerViewMobile extends StatelessWidget {
                               Text(
                                 "Weekly scheduled post designs, maintained professional website to help you be the best in your league!",
                                 style: kSectionSubheadingTextStyle,
-                                // textAlign: TextAlign.center,
                               ),
                             ],
                           ),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-                Container(
+                    ],
+                  ),
+                  Container(
                     width: double.infinity,
                     child: Image.asset(
                       "assets/images/posts2.png",
                       fit: BoxFit.cover,
-                    )),
-              ],
-            ),
-          ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/screens/web_view/empower_view_web.dart
+++ lib/screens/web_view/empower_view_web.dart
@@ -13,78 +13,73 @@ class EmpowerViewWeb extends StatelessWidget {
       child: Container(
         padding: EdgeInsets.symmetric(
             horizontal: screenWidth / 40, vertical: screenHeight / 10),
-        // width: screenWidth,
-        // height: screenHeight,
+        width: screenWidth,
         color: WhitebackgroundCustomcolor,
-        child: Column(
-          children: [
-            Padding(
-              padding: EdgeInsets.only(left: screenWidth / 25),
-              child: Row(
-                children: [
-                  Container(
-                    height: 150,
-                    width: 120,
-                    child: Image.asset(
-                      "assets/images/square.png",
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 16,
-                  ),
-                  Text(
-                    "Empower",
-                    style: kSectionHeadingTextStyle.copyWith(
-                        color: Colors.grey.shade400, fontSize: 130),
-                  )
-                ],
-              ),
-            ),
-            SizedBox(
-              height: screenHeight / 40,
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: screenWidth / 20),
-              child: Text(
-                'Solutions that Fit Your Budget and Needs. We understand the challenges faced by tier 3 or low-level clubs with limited resources. Our website development and social media post designing service is tailored to address your pain points while ensuring affordability and quality. With us, you can enhance your online presence without breaking the bank.',
-                textAlign: TextAlign.justify,
-                style: kSectionSubheadingTextStyle.copyWith(
-                    fontSize: 30, fontWeight: FontWeight.w400),
-              ),
-            ),
-            SizedBox(
-              height: screenHeight / 20,
-            ),
-            Row(
-              children: [
-                Expanded(
-                  child: Stack(
-                    children: [
-                      Positioned(
-                        right: 490,
-                        bottom: 200,
-                        height: 180,
-                        width: 170,
-                        child: Image.asset(
-                          "assets/images/rectangle.png",
-                          // fit: BoxFit.cover,
-                        ),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(left: screenWidth / 25),
+                child: Row(
+                  children: [
+                    Container(
+                      height: 150,
+                      width: 120,
+                      child: Image.asset(
+                        "assets/images/square.png",
                       ),
-                      Padding(
-                        padding: EdgeInsets.only(left: screenWidth / 10),
-                        child: Container(
-                          padding: EdgeInsets.only(top: screenHeight / 8),
-                          width: 400,
-                          height: 400,
-                          // color: Colors.grey.shade300,
-                          child: const Positioned.fill(
+                    ),
+                    const SizedBox(
+                      width: 16,
+                    ),
+                    Text(
+                      "Empower",
+                      style: kSectionHeadingTextStyle.copyWith(
+                          color: Colors.grey.shade400, fontSize: 130),
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: screenHeight / 40,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: screenWidth / 20),
+                child: Text(
+                  'Solutions that Fit Your Budget and Needs. We understand the challenges faced by tier 3 or low-level clubs with limited resources. Our website development and social media post designing service is tailored to address your pain points while ensuring affordability and quality. With us, you can enhance your online presence without breaking the bank.',
+                  textAlign: TextAlign.justify,
+                  style: kSectionSubheadingTextStyle.copyWith(
+                      fontSize: 30, fontWeight: FontWeight.w400),
+                ),
+              ),
+              SizedBox(
+                height: screenHeight / 20,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Stack(
+                      children: [
+                        Positioned(
+                          right: 480,
+                          bottom: 120,
+                          height: 180,
+                          width: 170,
+                          child: Image.asset(
+                            "assets/images/rectangle.png",
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(left: screenWidth / 10),
+                          child: Container(
+                            padding: EdgeInsets.only(top: screenHeight / 8),
+                            width: 400,
                             child: Column(
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
                                 Text(
                                   "Professional Club Website",
                                   style: kSectionHeadingTextStyle,
-                                  // textAlign: TextAlign.center,
                                 ),
                                 SizedBox(
                                   height: 16,
@@ -92,54 +87,47 @@ class EmpowerViewWeb extends StatelessWidget {
                                 Text(
                                   "Weekly scheduled post designs, maintained professional website to help you be the best in your league!",
                                   style: kSectionSubheadingTextStyle,
-                                  // textAlign: TextAlign.center,
                                 ),
                               ],
                             ),
                           ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-                Expanded(
-                  child: Image.asset("assets/images/website.png"),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: screenHeight / 20,
-            ),
-            Row(
-              children: [
-                Expanded(
-                  child: Stack(
-                    children: [
-                      Positioned(
-                        right: 490,
-                        bottom: 200,
-                        height: 180,
-                        width: 170,
-                        child: Image.asset(
-                          "assets/images/rectangle.png",
-                          // fit: BoxFit.cover,
+                  Expanded(
+                    child: Image.asset("assets/images/website.png"),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: screenHeight / 20,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Stack(
+                      children: [
+                        Positioned(
+                          right: 480,
+                          bottom: 120,
+                          height: 180,
+                          width: 170,
+                          child: Image.asset(
+                            "assets/images/rectangle.png",
+                          ),
                         ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(left: screenWidth / 10),
-                        child: Container(
-                          padding: EdgeInsets.only(top: screenHeight / 8),
-                          width: 400,
-                          height: 400,
-                          // color: Colors.grey.shade300,
-                          child: const Positioned.fill(
+                        Padding(
+                          padding: EdgeInsets.only(left: screenWidth / 10),
+                          child: Container(
+                            padding: EdgeInsets.only(top: screenHeight / 8),
+                            width: 400,
                             child: Column(
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
                                 Text(
                                   "Social Media Post Designs",
                                   style: kSectionHeadingTextStyle,
-                                  // textAlign: TextAlign.center,
                                 ),
                                 SizedBox(
                                   height: 16,
@@ -147,22 +135,21 @@ class EmpowerViewWeb extends StatelessWidget {
                                 Text(
                                   "Weekly scheduled post designs, maintained professional website to help you be the best in your league!",
                                   style: kSectionSubheadingTextStyle,
-                                  // textAlign: TextAlign.center,
                                 ),
                               ],
                             ),
                           ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-                Expanded(
-                  child: Image.asset("assets/images/posts.png"),
-                ),
-              ],
-            ),
-          ],
+                  Expanded(
+                    child: Image.asset("assets/images/posts.png"),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
